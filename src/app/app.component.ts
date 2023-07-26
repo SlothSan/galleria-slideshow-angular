25b727ce7c5e160806cfd2d4d7db85f57a819110
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-07-26 22:06:30 +0100

    feature: WIP header creation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'galleria-root',
   templateUrl: './app.component.html',
-  styleUrls: [],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'galleria-slideshow-angular';
