577a54c2bed00aa9b31ed960f27b15b76b99b435
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-07-26 23:18:06 +0100

    chore: refactored icons.

---
+++ src/app/shared/icons/icons/icons.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'galleria-icons',
+  templateUrl: './icons.component.html',
+  styleUrls: []
+})
+export class IconsComponent {
+
+}
