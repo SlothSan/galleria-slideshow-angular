22fdd230b444c5fc12228a2ae3b538137d11f055
Mike Oram <60877515+SlothSan@users.noreply.github.com>
2023-07-27 20:35:30 +0100

    created gallery-card

---
+++ src/app/core/gallery/gallery-card/gallery-card.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'galleria-gallery-card',
+  templateUrl: './gallery-card.component.html',
+  styleUrls: ['./gallery-card.component.scss']
+})
+export class GalleryCardComponent {
+  @Input() thumbnailPainting: string = '';
+  @Input() paintingTitle: string = '';
+  @Input() paintingArtist: string = '';
+  constructor() {
+  }
+}
